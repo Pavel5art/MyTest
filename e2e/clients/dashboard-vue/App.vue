bbf410c6521e503e4747264f828c718408b6c486
Pavel5art <pavel5patyi@gmail.com>
2025-12-25 11:38:01 +0100

    Experimenting e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :TestJS="TestJS" />
+</template>
+
+<script>
+import Vue from 'vue'
+import TestJS from '@TestJS/core'
+import { Dashboard } from '@TestJS/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    TestJS: () => new TestJS(),
+  },
+}
+</script>
+
+<style src="@TestJS/core/dist/style.css"></style>
+<style src="@TestJS/dashboard/dist/style.css"></style>
